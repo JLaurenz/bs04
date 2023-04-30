6a212b78468888da31a2a8ba52f48b25ee4aa276
John Angelo <67223472+john-angelo1610@users.noreply.github.com>
2023-04-30 21:30:44 +0800

    Added Admin Dashboard
    Only added Admin Dashboard for now in case need to change the layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Login from './components/Login.vue';
 import Registration from './components/Registration.vue';
+import Admin from './components/Admin.vue';
 </script>
 
 <template>
   <Login />
   <Registration />
+  <Admin />
 </template>
 
 <style scoped>
---
+++ src/components/Admin.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+    msg: String,
+})
+
+const count = ref(0)
+</script>
+    
+<template>
+    <div class="admin-dashboard mt-5">
+        <div class="admin-dashboard-icon primary-bg">
+            <span><i class="fa fa-tachometer"></i></span>
+        </div>
+        <h3 class="mb-3">Admin Dashboard</h3>
+        <div class="d-grid">
+            <a href="" class="btn primary-bg">User Management</a>
+            <a href="" class="btn primary-bg">Student Info Management</a>
+            <a href="" class="btn primary-bg">Document Management</a>
+            <a href="" class="btn primary-bg">Appoinrment Schedule</a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .primary-bg {
+        background-color: #5891ff;
+    }
+    .admin-dashboard{
+        background-color: #fff;
+        max-width: 100%;
+        margin: auto;
+        padding: 50px 70px;
+        border-radius: 30px;
+        box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.075);
+    }
+
+    .admin-dashboard .admin-dashboard-icon{
+        text-align: center;
+        border-radius: 50%;
+        font-size: 40px;
+        color: white;
+        width: 100px;
+        height: 100px;
+        margin: auto;
+        margin-bottom: 50px;
+        line-height: 100px;
+    }
+
+    .admin-dashboard a{
+        border-radius: 30px;
+        padding: 10px 20px;
+        font-size: 18px;
+        font-weight: bold;
+        border: none;
+        color: white;
+        margin-top: 20px;
+    }
+
+    a:hover{
+        background-color: #1F69FF;
+    }
+</style>
